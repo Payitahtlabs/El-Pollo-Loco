307ea2da37abb861b940df5b5f0657967bf24c5e
Mustafa <maveralabs@gmail.com>
2026-04-03 21:03:28 +0200

    refactor: add image preloading for character walk animation
    - Add IMAGES_WALKING array to Character with all 6 walk frames
    - Add loadImages() method to MovableObject to preload multiple images into imageCache
    - Add imageCache property to MovableObject

--- models/character.class.js
+++ models/character.class.js
@@ -3,9 +3,19 @@ class Character extends MovableObject {
     height = 280;
     y = 155;
 
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ];
+
     constructor() {
         super();
-        this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
     }
 
     jump() {
