f97421b5895a68d8137f1aef5147a30281b5c3e0
Mustafa <maveralabs@gmail.com>
2026-04-11 22:43:56 +0200

    feat: add jump physics and jumping animation for character
    - add reusable gravity, vertical speed, and jump logic to MovableObject
    - define character ground position and jump input handling
    - trigger jumps via arrow up or space on key press
    - preload jumping frames and prioritize jump animation while airborne
    - keep walking animation only for grounded movement

--- models/character.class.js
+++ models/character.class.js
@@ -3,9 +3,11 @@ class Character extends MovableObject {
     x = 220;
     height = 280;
     y = 155;
+    groundY = 155;
     speed = 240;
     isMoving = false;
     wasMoving = false;
+    jumpKeyPressed = false;
 
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -16,18 +18,28 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png',
     ];
 
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ];
+
     constructor() {
         super();
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-    }
-
-    jump() {
-
+        this.loadImages(this.IMAGES_JUMPING);
     }
 
     update(deltaTime, keyboard, level) {
         this.isMoving = keyboard.RIGHT || keyboard.LEFT;
+        this.applyGravity(deltaTime);
 
         if (keyboard.RIGHT) {
             this.otherDirection = false;
@@ -39,6 +51,8 @@ class Character extends MovableObject {
             this.moveLeft(deltaTime);
         }
 
+        this.handleJumpInput(keyboard);
+
         if (this.x < 0) {
             this.x = 0;
         }
@@ -48,7 +62,26 @@ class Character extends MovableObject {
         }
     }
 
+    handleJumpInput(keyboard) {
+        let jumpKeyActive = keyboard.UP || keyboard.SPACE;
+
+        if (jumpKeyActive && !this.jumpKeyPressed) {
+            this.jump();
+        }
+
+        this.jumpKeyPressed = jumpKeyActive;
+    }
+
     animate(deltaTime) {
+        if (this.isAboveGround()) {
+            this.wasMoving = false;
+
+            if (this.isAnimationFrameDue(deltaTime)) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            }
+            return;
+        }
+
         if (!this.isMoving) {
             this.img = this.imageCache[this.IMAGES_WALKING[0]];
             this.currentImage = 0;
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,6 +1,7 @@
 class MovableObject {
     x = 120;
 	y = 280;
+    groundY = 280;
     height = 150;
     width = 100;
     img;
@@ -9,6 +10,9 @@ class MovableObject {
     animationCounter = 0;
     animationFps = 10;
     speed = 0;
+    speedY = 0;
+    jumpStrength = 900;
+    gravity = 2200;
     otherDirection = false;
 
     loadImage(path) {
@@ -45,5 +49,30 @@ class MovableObject {
 
     moveLeft(deltaTime) {
         this.x -= this.speed * deltaTime;
+    }
+
+    applyGravity(deltaTime) {
+        if (!this.isAboveGround() && this.speedY >= 0) {
+            this.y = this.groundY;
+            this.speedY = 0;
+            return;
+        }
+
+        this.y += this.speedY * deltaTime;
+        this.speedY += this.gravity * deltaTime;
+
+        if (this.y > this.groundY) {
+            this.y = this.groundY;
+            this.speedY = 0;
+        }
+    }
+
+    isAboveGround() {
+        return this.y < this.groundY;
+    }
+
+    jump() {
+        if (this.isAboveGround()) return;
+        this.speedY = -this.jumpStrength;
     }
 }
